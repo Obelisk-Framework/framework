4869b710ca78710d28c2593420fde6d81913702b
AndiLfl <andreas.l1@gmx.ch>
2026-08-11 04:23:20 +0700

    chore: remove Notifications.vue, extracted to oblsk_notifications

--- web/src/globalElements.js
+++ web/src/globalElements.js
@@ -1,7 +1,5 @@
-import Notifications from './components/global/Notifications.vue'
 import ProgressBars from './components/global/ProgressBars.vue'
 
 export default [
-  { name: 'notifications', component: Notifications, defaultVisible: true },
   { name: 'progressBars', component: ProgressBars, defaultVisible: true }
 ]
